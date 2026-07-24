39aac1d069ee3696f61b027abbe85f99e9763585
Piston <piston@aegisos.cc>
2026-07-24 20:13:30 +0000

    content: founder content update [piston]

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -50,7 +50,7 @@ export default function Hero() {
               </motion.div>
               <motion.div variants={itemVariants}>
                 <h1 className="hero-headline">
-                  The First Autonomous Asset Class
+                  The First Autonomous Asset Class for Institutions
                 </h1>
               </motion.div>
               <motion.div variants={itemVariants} className="hero-subhead-wrap">
